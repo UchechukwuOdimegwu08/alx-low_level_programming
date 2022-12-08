1991fc48957efb7ba2a6f1d62accf27a26d9fa94
UchechukwuOdimegwu08 <meucebere@gmail.com>
2022-12-08 12:07:29 -0800

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                               
+#include <studio.h>
 /**
- * main - Entry 
- *Return: Always 0                                                            
+ * main - Entry
+ * Return: Always 0
  */
-int main(void)                                                                  {
-	puts("\"Programming is like building a multi lingual puzzle);
+int main(void)
+{
+	puts("\"Programming is like building a multi lingual puzzle");
 	return (0);
 }
-
