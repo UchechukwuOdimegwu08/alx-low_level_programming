508f2eaf5e1dda9e6fd27abdf50aeba689d42cb3
UchechukwuOdimegwu08 <meucebere@gmail.com>
2022-12-16 01:52:27 -0800

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,11 +3,11 @@
 /**
  * _isupper - checks for uppercase character
  * @c: Variable text
- * Return: Always 0.
+ * Return: 1 if true, 0 if false
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= "Z")
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
