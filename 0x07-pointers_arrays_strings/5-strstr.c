56b26b85b4068d818a99c2f308f69970bd37a0d0
UchechukwuOdimegwu08 <meucebere@gmail.com>
2023-01-03 01:48:30 -0800

    5-strstr task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,12 +11,13 @@ for (; *haystack != '\0'; haystack++)
 {
 	char *l = haystack;
 	char *p = needle;
-	{
-		l++;
-		p++;
-	}
-	if (*p == '\0')
-		return (haystack);
+while (*l == *p && *p != '\0')
+{
+	l++;
+	p++;
+}
+if (*p == '\0')
+return (haystack);
 }
 return (0);
 }
