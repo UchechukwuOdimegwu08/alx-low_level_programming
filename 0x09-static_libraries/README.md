e71b71688919b827788edf7a49ddc67611d08873
UchechukwuOdimegwu08 <meucebere@gmail.com>
2023-01-06 12:50:15 -0800

    A library is not a luxury

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,3 +1,2 @@
 Task 0x09. C - Static libraries
 A static library is a compiled object file containing all symbols required by the main program to operate in the C programming. In this task, a static C library will be created using Using "ar" and "ranlib".
-
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -9,6 +8,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
+int main(void);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -21,5 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
