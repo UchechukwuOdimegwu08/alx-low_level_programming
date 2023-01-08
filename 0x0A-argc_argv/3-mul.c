3b97f1ccc86aaa1e7956f05c42271104e672b8dd
UchechukwuOdimegwu08 <meucebere@gmail.com>
2023-01-07 23:44:47 -0800

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers
+ * @argc: the argument count in array
+ * @argv: the argument vector in array
+ *
+ * Return: return to 0
+ */
+int main(int argc, char *argv[])
+{
+	int index, multiplication;
+
+	multiplication = 2;
+	if (argc < 4)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
